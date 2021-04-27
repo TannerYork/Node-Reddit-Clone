d29c7752528810f584b717810e3825413ea0be84
TannerYork <tannerwyork@gmail.com>
2021-04-27 11:10:44 -0500

    Add feature for allowing users to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,29 +3,60 @@ const User = require("../models/User");
 
 module.exports = function(app) {
 
-    app.get('/sign-up', (reg, res) => {
-        res.render('sign-up');
-    });
+  app.get('/sign-up', (reg, res) => {
+    res.render('sign-up');
+  });
 
-    app.post('/sign-up', (req, res) => {
-        const user = new User(req.body)
+  app.post('/sign-up', (req, res) => {
+    const user = new User(req.body)
 
-        user
-          .save()
-          .then(user => {
-              var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: '60 days' });
-              res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-              res.redirect('/');
-          })
-          .catch(err => {
-              console.log(err.message)
-              return res.status(400).send({ err: err });
-          });
+    user
+      .save()
+      .then(user => {
+        var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: '60 days' });
+        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+          res.redirect('/');
+        })
+        .catch(err => {
+          console.log(err.message)
+          return res.status(400).send({ err: err });
+        });
     });
 
     app.get('/logout', (req, res) => {
-        res.clearCookie('nToken');
-        res.redirect('/');
+      res.clearCookie('nToken');
+      res.redirect('/');
+    });
+
+    app.get('/login', (req, res) => {
+      res.render('login');
+    });
+
+    app.post('/login', (req, res) => {
+      const { username, password } = req.body;
+
+      User.findOne({ username }, 'username password')
+        .then(user => {
+          if (!user) {
+            return res.status(401).send({message: "Wronger Username or Password" });
+          }
+
+          user.comparePassword(password, (err, isMatch) => {
+            if(!isMatch) {
+              return res.status(401).send({message: "Wronger Username or Password" });
+            }
+
+            const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+              expiresIn: '60 days'
+            })
+
+            res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+            res.redirect('/');
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
     });
 
 }

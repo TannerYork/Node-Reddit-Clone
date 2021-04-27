72a485815016882a0cd9e21be1459dca8478824b
TannerYork <tannerwyork@gmail.com>
2021-04-27 10:41:51 -0500

    Add feature for alowing users to create an account

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 dotenv.config()
 
 
@@ -21,9 +23,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
+// Cookie Parser
+app.use(cookieParser());
+
 // Setup Controllers
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
+require('./controllers/auth.js')(app);
 
 // Listen to Port
 app.listen(process.env.PORT, () => {

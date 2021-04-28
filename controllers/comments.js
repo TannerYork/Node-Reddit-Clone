cab2f2fc116f5e5f823af71e022f5c9cda4b113e
TannerYork <tannerwyork@gmail.com>
2021-04-28 16:09:38 -0500

    Add feature for show author of post and comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,23 +5,28 @@ module.exports = function(app) {
     // CREATE Comment
     app.post("/posts/:postId/comments", function(req, res) {
       // INSTANTIATE INSTANCE OF MODEL
-      const comment = new Comment(req.body);
+      if (req.user) {
+        const comment = new Comment(req.body);
+        comment.author = req.user._id;
 
-      // SAVE INSTANCE OF Comment MODEL TO DB
-      comment
-        .save()
-        .then(comment => {
-            return Post.findById(req.params.postId);
-        })
-        .then(post => {
-            post.comments.unshift(comment);
-            return post.save();
-        })
-        .then(post => {
-            res.redirect(`/`);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+        // SAVE INSTANCE OF Comment MODEL TO DB
+        comment
+          .save()
+          .then(comment => {
+              return Post.findById(req.params.postId);
+          })
+          .then(post => {
+              post.comments.unshift(comment);
+              return post.save();
+          })
+          .then(post => {
+              res.redirect(`/`);
+          })
+          .catch(err => {
+              console.log(err);
+          });
+      } else {
+        return res.status(401); // UNAUTHORIZED
+      }
     });
 }
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,49 +1,69 @@
 const Post = require('../models/Post.js');
+const User = require('../models/User.js');
 
 module.exports = function(app) {
-    app.get('/', (req, res) => {
-        Post.find({}).lean()
-        .then(posts => {
-            res.render('posts-index', { posts });
+  app.get('/', (req, res) => {
+    Post.find({}).lean().populate('author')
+      .then(posts => {
+        res.render('posts-index', { posts });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
+  
+  app.get('/posts/new', (reg, res) => {
+    return res.render('new-post');
+  });
+  
+  app.post('/posts/new', (req, res) => {
+    if (req.user) {
+      const post  = new Post(req.body);
+      post.author = req.user._id;
+      post
+        .save()
+        .then(post => {
+          return User.findById(req.user._id);
+        })
+        .then(user => {
+          user.posts.unshift(post);
+          user.save();
+          res.redirect(`/posts/${post._id}`);
         })
         .catch(err => {
-            console.log(err.message);
+          console.log(err.message);
         });
-    });
-    
-    app.get('/posts/new', (reg, res) => {
-        return res.render('new-post');
-    });
-    
-    app.post('/posts/new', (req, res) => {
-        if (req.user) {
-            const post  = new Post(req.body);
-            post.save((err, post) => {
-                return res.redirect('/');
-            });
-        } else {
-            return res.status(401); // UNAUTHORIZED
-        }
-    });
-    
-    app.get("/posts/:id", (req, res) => {
-        // LOOK UP THE POST
-        Post.findById(req.params.id).lean().populate('comments').then((post) => {
-            res.render('posts-show', { post })
-        })
-        .catch(err => {
-            console.log(err.message);
-        });
-    });
+    } else {
+      return res.status(401); // UNAUTHORIZED
+    }
+  });
+  
+  app.get("/posts/:id", (req, res) => {
+    // LOOK UP THE POST
+    Post.findById(req.params.id).lean()
+      .populate('author')
+      .populate({ 
+        path: 'comments',
+          populate: {
+            path: 'author'
+          } 
+      })
+      .then((post) => {
+        res.render('posts-show', { post })
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
 
-    // SUBREDDIT
-    app.get("/n/:subreddit", (req, res) => {
-        Post.find({ subreddit: req.params.subreddit }).lean()
-          .then(posts => {
-            res.render("posts-index", { posts });
-          })
-          .catch(err => {
-            console.log(err);
-          });
-    });
+  // SUBREDDIT
+  app.get("/n/:subreddit", (req, res) => {
+    Post.find({ subreddit: req.params.subreddit }).lean().populate('author')
+      .then(posts => {
+        res.render("posts-index", { posts });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
 };

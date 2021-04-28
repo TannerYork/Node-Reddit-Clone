b5f630206ed602d06b5a34a4b77abbbe816e9497
TannerYork <tannerwyork@gmail.com>
2021-04-28 16:28:12 -0500

    Update posts tests to accound for new author field

--- test/auth.js
+++ test/auth.js
@@ -1,19 +1,19 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const server = require("../server");
+const server = require("../index");
 const should = chai.should();
 chai.use(chaiHttp);
 
 // Agent that will keep track of our cookies
 const agent = chai.request.agent(server);
 
-const User = require("../models/user");
+const User = require("../models/User.js");
 
 describe("User", function() {
   after(function () {
     agent.close()
   });
-  
+
   it("should not be able to login if they have not registered", function(done) {
     agent.post("/login", { email: "wrong@wrong.com", password: "nope" }).end(function(err, res) {
       res.status.should.be.equal(401);
--- test/posts.js
+++ test/posts.js
@@ -1,49 +1,84 @@
-const app = require("./../server");
+const app = require("./../index");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
+const agent = chai.request.agent(app);
 
-const Post = require('../models/post');
-const server = require('../server');
+const Post = require('../models/Post.js');
+const User = require("../models/User.js");
 
 chai.should();
 chai.use(chaiHttp);
 
 describe('Posts', function() {
-  const agent = chai.request.agent(server);
   // Post that we'll use for testing purposes
   const newPost = {
-      title: 'post title',
-      url: 'https://www.google.com',
-      summary: 'post summary'
+    title: 'post title',
+    url: 'https://www.google.com',
+    summary: 'post summary',
+    subreddit: 'funny'
   };
+  const user = {
+    username: 'poststest',
+    password: 'testposts'
+  };
+
+  before(function (done) {
+    agent
+      .post('/sign-up')
+      .set("content-type", "application/x-www-form-urlencoded")
+      .send(user)
+      .then(function (res) {
+        done();
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
+
   it('should create with valid attributes at POST /posts/new', function (done) {
     Post.estimatedDocumentCount()
-        .then(function (initialDocCount) {
-            agent
-                .post('/posts/new')
-                .set('content-type', 'application/x-www-form-urlencoded')
-                .send(newPost)
-                .then(function(res) {
-                    Post.estimatedDocumentCount()
-                        .then(function(newDocCount) {
-                            expect(res).to.have.status(200);
-                            expect(newDocCount).to.be.equal(initialDocCount + 1);
-                            done();
-                        })
-                        .catch(function(err) {
-                            done(err);
-                        });
-                })
-                .catch(function(err) {
-                    done(err);
-                });
-        })
-        .catch(function(err) {
+      .then(function (initialDocCount) {
+        agent
+          .post('/posts/new')
+          .set('content-type', 'application/x-www-form-urlencoded')
+          .send(newPost)
+          .then(function(res) {
+            Post.estimatedDocumentCount()
+              .then(function(newDocCount) {
+                expect(res).to.have.status(200);
+                expect(newDocCount).to.be.equal(initialDocCount + 1);
+                done();
+              })
+              .catch(function(err) {
+                done(err);
+              });
+          })
+          .catch(function(err) {
             done(err);
-        });
+          });
+      })
+      .catch(function(err) {
+        console.log('========error=========')
+        done(err);
+      });
   });
-  after(function () {
-    Post.findOneAndDelete(newPost);
+
+  after(function (done) {
+    Post.findOneAndDelete(newPost)
+    .then(function (res) {
+      agent.close()
+  
+      User.findOneAndDelete({username: user.username})
+        .then(function (res) {
+          done()
+        })
+        .catch(function (err) {
+          done(err);
+        });
+    })
+    .catch(function (err) {
+      done(err);
+    });
   });
 });
